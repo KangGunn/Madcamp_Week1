83a4eda9ea61787c5dd7d71501f8e90e6a6a61a5
minwookang219 <115993740+minwookang219@users.noreply.github.com>
2024-12-27 16:44:20 +0900

    Merge version - error 발생(앱이 안켜짐)

---
+++ app/src/main/java/ItemData.java
@@ -0,0 +1,28 @@
+package com.example.restart;
+public class ItemData {
+    private int imageResId;  // 이미지 리소스 ID
+    private String text1;    // 텍스트1
+    private String text2;    // 텍스트2
+
+    // 생성자
+    public ItemData(int imageResId, String text1, String text2) {
+        this.imageResId = imageResId;
+        this.text1 = text1;
+        this.text2 = text2;
+    }
+
+    // Getter 메서드
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public String getText1() {
+        return text1;
+    }
+
+    public String getText2() {
+        return text2;
+    }
+}
+
+
