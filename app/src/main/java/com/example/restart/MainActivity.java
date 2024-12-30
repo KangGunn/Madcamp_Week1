1b85c3f11ff70178cc781a91f7f002d6d3c68d0d
minwookang219 <115993740+minwookang219@users.noreply.github.com>
2024-12-31 00:01:56 +0900

    리스트 추가삭제 하려고 하는데 어려워요

--- app/src/main/java/com/example/restart/MainActivity.java
+++ app/src/main/java/com/example/restart/MainActivity.java
@@ -1,14 +1,8 @@
 package com.example.restart;
 
 import android.os.Bundle;
-import android.widget.ListView;
-import com.example.restart.ItemData;
-import com.example.restart.CustomAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
